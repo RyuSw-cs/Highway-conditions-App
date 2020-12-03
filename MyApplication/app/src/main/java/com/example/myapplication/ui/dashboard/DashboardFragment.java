c4cadef98d4bad248976b9825a5e446e9dfb5303
EvergreenTree97 <tkdfhr7497@naver.com>
2020-12-04 01:50:03 +0900

    노선설정 상하행구분 스피너

--- MyApplication/app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,12 +1,17 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,12 +25,34 @@ import com.example.myapplication.R;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
-
+    private Context mContext;
+    Spinner spinner, spinner2;
+    ArrayAdapter<CharSequence> adspin, adspin2;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_traffic, container, false);
+
+        spinner = (Spinner) root.findViewById(R.id.spinner2);
+        spinner.setPrompt("노선을 선택하세요.");
+        spinner2 = (Spinner) root.findViewById(R.id.spinner3);
+        spinner.setPrompt("상/하행을 선택하세요.");
+        adspin = ArrayAdapter.createFromResource(mContext, R.array.road, android.R.layout.simple_spinner_item);
+        adspin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adspin);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?>  parent, View view, int position, long id) {}
+            public void onNothingSelected(AdapterView<?>  parent) {}
+        });
+        adspin2 = ArrayAdapter.createFromResource(mContext, R.array.updown, android.R.layout.simple_spinner_item);
+        adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adspin2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?>  parent, View view, int position, long id) {}
+            public void onNothingSelected(AdapterView<?>  parent) {}
+        });
+
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -34,5 +61,10 @@ public class DashboardFragment extends Fragment {
             }
         });
         return root;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
